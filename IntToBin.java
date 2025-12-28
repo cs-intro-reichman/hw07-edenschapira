69abccd07719dd927dd220a0f87440749e257a01
Eden Schapira <eden.schapira@post.runi.ac.il>
2025-12-28 16:29:44 +0200

    IntToBin test 1

--- IntToBin.java
+++ IntToBin.java
@@ -2,13 +2,25 @@
 public class IntToBin {
 
     public static void main(String[] args) {
-        int x = Integer.parseInt(args[0]);
+        // int x = Integer.parseInt(args[0]);
+        int x =0 ;
         System.out.println("Binary representation of " + x + " is: " + toBinary(x));
     }
 
     /** Returns the binary representation of the given integer, as a string of 0's and 1's.  */
     public static String toBinary(int x) {
-        //// Replace the following statement with your code
-        return "";
-    }    
+        if(x == 0){
+            return "0";
+        }
+        return toBinaryHelper(x);
+    } 
+
+    private static String toBinaryHelper(int x){
+        if(x == 0){
+            return "";
+        }
+        String current = (x % 2 == 0) ? "0" : "1";
+        return toBinaryHelper(x / 2) + current;  
+    }   
+
  }
